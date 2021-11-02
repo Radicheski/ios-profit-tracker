6860f3a3654ddd78e590213ec0eb3cc90f4ab152
Radicheski <erik.radicheski@gmail.com>
2021-11-01 21:57:40 -0300

    Created PortfolioNavigationController

---
+++ finance/ViewController/PortfolioNavigationController.swift
@@ -0,0 +1,22 @@
+//
+//  PortfolioNavigationController.swift
+//  finance
+//
+//  Created by Erik Radicheski da Silva on 28/10/21.
+//
+
+import UIKit
+
+class PortfolioNavigationController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Portfolio"
+        self.tabBarItem.image = UIImage(systemName: "globe")
+        self.pushViewController(PortfolioViewController(), animated: true)
+    }
+
+
+}
+
