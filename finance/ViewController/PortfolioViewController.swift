c4661372cbce60633917d5cf95a982081011a001
Radicheski <erik.radicheski@gmail.com>
2021-11-01 21:57:03 -0300

    Added method to format table view footer

--- finance/ViewController/PortfolioViewController.swift
+++ finance/ViewController/PortfolioViewController.swift
@@ -15,7 +15,18 @@ class PortfolioViewController: BaseViewController<PortfolioView> {
         super.viewDidLoad()
         self.customView.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "portfolioItem")
         self.title = "Global portfolio"
-        self.customView.tableView.dataSource = self.datasourc
+        self.customView.tableView.dataSource = self.datasource
+        self.customView.tableView.tableFooterView = self.getFooter()
+    }
+    
+    func getFooter() -> UIView {
+        let footer = UILabel()
+        footer.backgroundColor = .yellow
+        footer.textAlignment = .center
+        footer.frame = CGRect(x: 0, y: 0, width: self.customView.tableView.frame.width, height: 40)
+        let value = PortfolioMock.shared.data[0].items.map ( { $0.weight } ).reduce(Decimal(), { $0 + $1 } )
+        footer.text = "Unallocated: \(100 - value * 100)%"
+        return footer
     }
 
 
