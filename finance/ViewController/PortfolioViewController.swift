cd446b9f0317dd100717185fbd73a907707f7914
Radicheski <erik.radicheski@gmail.com>
2021-11-03 19:05:54 -0300

    Extracted FlatPortfolioDataSource class

--- finance/ViewController/PortfolioViewController.swift
+++ finance/ViewController/PortfolioViewController.swift
@@ -24,7 +24,7 @@ class PortfolioViewController: BaseViewController<PortfolioView> {
         }
     }
 
-    let flatdatasource = FlatPortfolioDataSouce()
+    let flatdatasource = FlatPortfolioDataSource()
 
     var parentId: UUID?
 
@@ -81,29 +81,6 @@ extension PortfolioViewController: UITableViewDelegate {
             vc.title = selectedItem.name
             self.navigationController?.pushViewController(vc, animated: true)
         }
-    }
-    
-}
-
-class FlatPortfolioDataSouce: NSObject, UITableViewDataSource {
-    
-    var source: [PortfolioItem] {
-        get {
-            return PortfolioItem.getItems(PortfolioMock.shared.data).sorted(by: { $0.name < $1.name } )
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return source.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "portfolioItem", for: indexPath)
-        var conf = cell.defaultContentConfiguration()
-        conf.text = self.source[indexPath.row].name
-        conf.secondaryText = "\(self.source[indexPath.row].weight)"
-        cell.contentConfiguration = conf
-        return cell
     }
     
 }
