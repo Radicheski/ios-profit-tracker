4e0bf8d32e694585add49c7ae5f634ee372db5a5
Radicheski <erik.radicheski@gmail.com>
2021-11-03 18:57:47 -0300

    Implemented PortfolioManagerPresenterProtocol

--- finance/ViewController/PortfolioViewController.swift
+++ finance/ViewController/PortfolioViewController.swift
@@ -8,50 +8,66 @@
 import UIKit
 
 class PortfolioViewController: BaseViewController<PortfolioView> {
+
     var interactor: PortfolioManagerInteractorProtocol? = PortfolioMock.shared
+    var presenter: PortfolioManagerPresenterProtocol? = PortfolioManagerDataSource()
     var viewController: PortfolioManagerViewControllerProtocol? {
         get {
             return self
         }
         set {}
     }
+
+    var dataSource: UITableViewDataSource? {
+        get {
+            return self.presenter as? UITableViewDataSource
+        }
+    }
+
+    let flatdatasource = FlatPortfolioDataSouce()
+
     var parentId: UUID?
+
+    var data: [PortfolioItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.customView.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "portfolioItem")
         if self.title == nil { self.title = "Global Portfolio"}
-        if worker.parentId == nil { self.customView.segmentedControl.isHidden = false }
-        self.customView.tableView.dataSource = self
+        if self.parentId == nil { self.customView.segmentedControl.isHidden = false }
+        self.customView.tableView.dataSource = self.dataSource
         self.customView.tableView.delegate = self
-        let value = self.worker.items.map( { $0.weight } ).reduce(Decimal(), { $0 + $1 } )
+        let value = self.data.map( { $0.weight } ).reduce(Decimal(), { $0 + $1 } )
         self.customView.footerText = "Unallocated: \(100 - value * 100)%"
+        self.customView.segmentedControlAction = { [unowned self] control in
+            if self.parentId == nil {
+                switch control.selectedSegmentIndex {
+                case 0:
+                    self.customView.tableView.dataSource = self.dataSource
+                    self.customView.tableView.delegate = self
+                    let value = self.data.map( { $0.weight } ).reduce(Decimal(), { $0 + $1 } )
+                    self.customView.footerText = "Unallocated: \(100 - value * 100)%"
+                case 1:
+                    self.customView.tableView.dataSource = self.flatdatasource
+                    self.customView.tableView.delegate = nil
+                    let value = self.flatdatasource.source.map( { $0.weight } ).reduce(Decimal(), { $0 + $1 } )
+                    self.customView.footerText = "Unallocated: \(100 - value * 100)%"
+                default: break
+                }
+                self.updateView()
+            }
+        }
         self.interactor?.loadData(parentId: self.parentId)
     }
     
     convenience init(parentId: UUID?) {
         self.init()
         self.parentId = parentId
-    }
-    
-    }
-    
-}
 
+        self.interactor?.presenter = self.presenter
+        self.presenter?.viewController = self
 
-extension PortfolioViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.worker.items.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "portfolioItem", for: indexPath)
-        var conf = cell.defaultContentConfiguration()
-        conf.text = self.worker.items[indexPath.row].name
-        conf.secondaryText = "\(self.worker.items[indexPath.row].weight)"
-        cell.contentConfiguration = conf
-        return cell
+        self.interactor?.loadData(parentId: parentId)
     }
     
 }
@@ -59,12 +75,35 @@ extension PortfolioViewController: UITableViewDataSource {
 extension PortfolioViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedItem = self.worker.items[indexPath.row]
-        let worker = PortfolioItemWorker(parentId: selectedItem.id)
-        if worker.items.isEmpty { return }
-        let vc = PortfolioViewController(worker: worker)
-        vc.title = selectedItem.name
-        self.navigationController?.pushViewController(vc, animated: true)
+        if let dataSource = self.presenter as? PortfolioManagerDataSource {
+            let selectedItem = dataSource.data[indexPath.row]
+            let vc = PortfolioViewController(parentId: selectedItem.id)
+            vc.title = selectedItem.name
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+}
+
+class FlatPortfolioDataSouce: NSObject, UITableViewDataSource {
+    
+    var source: [PortfolioItem] {
+        get {
+            return PortfolioItem.getItems(PortfolioMock.shared.data).sorted(by: { $0.name < $1.name } )
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return source.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "portfolioItem", for: indexPath)
+        var conf = cell.defaultContentConfiguration()
+        conf.text = self.source[indexPath.row].name
+        conf.secondaryText = "\(self.source[indexPath.row].weight)"
+        cell.contentConfiguration = conf
+        return cell
     }
     
 }
