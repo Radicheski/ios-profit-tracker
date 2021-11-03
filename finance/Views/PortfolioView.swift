ab5069876422371b4455f1c4d1892673ec2f4f0c
Radicheski <erik.radicheski@gmail.com>
2021-11-03 10:58:44 -0300

    Added UISegmentControl to PortfolioView

--- finance/Views/PortfolioView.swift
+++ finance/Views/PortfolioView.swift
@@ -11,6 +11,7 @@ class PortfolioView: UIView {
     
     let stackView = UIStackView()
     
+    let segmentedControl = UISegmentedControl()
     let tableView = UITableView()
     let footer = UILabel()
     
@@ -32,12 +33,23 @@ class PortfolioView: UIView {
         self.stackView.spacing = 0
         self.stackView.axis = .vertical
         self.stackView.distribution = .fill
+        self.stackView.alignment = .center
         self.stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             self.stackView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
             self.stackView.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor),
             self.stackView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
             self.stackView.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor),
+        ])
+        
+        self.segmentedControl.isHidden = true
+        self.segmentedControl.insertSegment(withTitle: "Tree", at: 0, animated: false)
+        self.segmentedControl.insertSegment(withTitle: "Flat", at: 1, animated: false)
+        self.stackView.addArrangedSubview(self.segmentedControl)
+        self.segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.segmentedControl.leftAnchor.constraint(equalTo: self.stackView.safeAreaLayoutGuide.leftAnchor, constant: 10),
+            self.segmentedControl.rightAnchor.constraint(equalTo: self.stackView.safeAreaLayoutGuide.rightAnchor, constant: -10),
         ])
         
         self.stackView.addArrangedSubview(self.tableView)
