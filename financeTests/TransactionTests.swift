e32c46937f0729c88cd1c3667a6d2e1f60d0f7a6
Radicheski <erik.radicheski@gmail.com>
2022-01-16 15:10:13 -0300

    Added TransactionTests

---
+++ financeTests/TransactionTests.swift
@@ -0,0 +1,42 @@
+//
+//  TransactionTests.swift
+//  financeTests
+//
+//  Created by Erik Radicheski da Silva on 14/01/22.
+//
+
+import XCTest
+import finance
+
+class TransactionTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    func testTest() throws {
+        var array: [Transaction] = []
+        if let path = Bundle.main.path(forResource: "transactions", ofType: "json"),
+           let data = FileManager.default.contents(atPath: path) {
+            let json = try JSON(data: data)
+            for object in json.arrayValue {
+                let item = Transaction()
+                #warning("Unsafe unwrap")
+                item.date = Formatter.shared.isoDate.date(from: object["date"].stringValue)!
+                item.quantity = object["quantity"].intValue
+                item.ticker = object["ticker"].stringValue
+                item.total = Decimal(string: object["total"].stringValue, locale: nil)!
+                array.append(item)
+            }
+        }
+        
+        var data: [String: Int] = [:]
+        for transaction in array {
+            data[transaction.ticker, default: 0] += transaction.quantity
+        }
+        data.keys.filter { data[$0]! == 0 }.forEach { data.removeValue(forKey: $0) }
+        data.forEach { print("\($0.key)\t\($0.value)") }
+        
+    }
+    
+}
