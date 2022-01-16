65b8bfa9ad0724ab0856e629e88fbc7d48a3732a
Radicheski <erik.radicheski@gmail.com>
2022-01-16 15:50:13 -0300

    Added Transaction class files

---
+++ finance/Model/Transaction/Transaction+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Transaction+CoreDataClass.swift
+//  finance
+//
+//  Created by Erik Radicheski da Silva on 14/01/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Transaction)
+public class Transaction: NSManagedObject {
+
+}
---
+++ finance/Model/Transaction/Transaction+CoreDataProperties.swift
@@ -0,0 +1,40 @@
+//
+//  Transaction+CoreDataProperties.swift
+//  finance
+//
+//  Created by Erik Radicheski da Silva on 14/01/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Transaction {
+
+    @nonobjc public class func createFetchRequest() -> NSFetchRequest<Transaction> {
+        return NSFetchRequest<Transaction>(entityName: "Transaction")
+    }
+
+    @NSManaged public var ticker: String
+    @NSManaged public var date: Date
+    @NSManaged public var quantity: Int
+    @NSManaged public var cdtotal: NSDecimalNumber
+
+}
+
+extension Transaction : Identifiable {
+
+}
+
+extension Transaction {
+    
+    convenience init() {
+        self.init(entity: Self.entity(), insertInto: nil)
+    }
+    
+    public var total: Decimal {
+        get { return self.cdtotal.decimalValue }
+        set { self.cdtotal = NSDecimalNumber(decimal: newValue) }
+    }
+}
