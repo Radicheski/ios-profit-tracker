1a1f2b5ba9132199ac945b4366a8502d4ce9822e
Radicheski <erik.radicheski@gmail.com>
2022-01-20 10:47:30 -0300

    Added Datastore protocols

---
+++ finance/Protocols/FullDatastore.swift
@@ -0,0 +1,15 @@
+//
+//  FullDatastore.swift
+//  finance
+//
+//  Created by Erik Radicheski da Silva on 20/01/22.
+//
+
+protocol FullDatastore: ReadOnlyDatastore {
+    
+    func newItem() -> T
+    func deleteItem(at index: Int)
+    func moveItem(at startIndex: Int, to endIndex: Int)
+    func saveChanges()
+    
+}
---
+++ finance/Protocols/ReadOnlyDatastore.swift
@@ -0,0 +1,16 @@
+//
+//  ReadDatastore.swift
+//  finance
+//
+//  Created by Erik Radicheski da Silva on 20/01/22.
+//
+
+protocol ReadOnlyDatastore {
+    associatedtype T
+    
+    var data: [T] { get }
+    var count: Int { get }
+    
+    func update()
+}
+
