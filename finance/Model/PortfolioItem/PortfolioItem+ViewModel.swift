b91c23b6bb572a8dbef43a9409dae628f15ff7bb
Radicheski <erik.radicheski@gmail.com>
2022-01-21 16:16:25 -0300

    Added PortfolioItem view model

--- finance/Model/PortfolioItem/PortfolioItem+ViewModel.swift
+++ finance/Model/PortfolioItem/PortfolioItem+ViewModel.swift
@@ -5,4 +5,31 @@
 //  Created by Erik Radicheski da Silva on 21/01/22.
 //
 
-import Foundation
+import UIKit
+
+extension PortfolioItem {
+    
+    class ViewModel {
+        var id: String
+        var name: String
+        var weight: String?
+        var parentId: String
+        var isAsset: Bool
+        
+        init(from portfolio: Portfolio) {
+            self.name = portfolio.name
+            self.weight = Formatter.shared.percent.string(from: portfolio.weight)
+            self.parentId = portfolio.parentId.uuidString
+            self.isAsset = portfolio.asset
+            self.id = portfolio.id.uuidString
+        }
+        
+        func configure(_ cell: UITableViewCell) {
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = self.name
+            contentConfiguration.secondaryText = self.weight
+            cell.contentConfiguration = contentConfiguration
+        }
+    }
+    
+}
