26935959e491fabbf39899c890c9c40b1a04eaf4
Radicheski <erik.radicheski@gmail.com>
2022-01-21 16:20:45 -0300

    Implemented new PortfolioView datasource and delegate

---
+++ finance/Features/PortfolioManager/Implementations/PortfolioTableViewDataSource.swift
@@ -0,0 +1,71 @@
+//
+//  PortfolioTableViewDataSource.swift
+//  finance
+//
+//  Created by Erik Radicheski da Silva on 21/01/22.
+//
+
+import UIKit
+
+class PortfolioTableViewDataSource: NSObject, UITableViewDataSource {
+    
+    private enum Sections: Int, CaseIterable {
+        case main
+        case accessory
+    }
+    
+    weak var viewController: PortfolioViewProtocol?
+    var data: [Portfolio.ViewModel] = []
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return Sections.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? self.data.count : tableView.isEditing ? 2 : 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = Sections(rawValue: indexPath.section)
+        let row = indexPath.row
+        let cell = tableView.dequeueReusableCell(UITableViewCell.self, for: indexPath)
+        switch section {
+        case .main:
+            self.data[row].configure(cell)
+        case .accessory:
+            var conf = cell.defaultContentConfiguration()
+            if tableView.isEditing && indexPath.row == 0 {
+                conf.text = CustomLocalization.PortfolioManager.globalPortfolioNewItem
+                conf.secondaryText = nil
+            } else {
+                conf.text = CustomLocalization.PortfolioManager.globalPortfolioUnallocated
+                conf.secondaryText = Formatter.shared.percent.string(from: self.data.map { try! Decimal($0.weight ?? "0", format: .percent) }.reduce(1, { $0 - $1 }))
+            }
+            cell.contentConfiguration = conf
+        default: break
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return indexPath.section == Sections.main.rawValue
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return indexPath.section == Sections.main.rawValue
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        self.viewController?.move(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if indexPath.section != 0 { return }
+        switch editingStyle {
+        case .delete:
+            self.viewController?.delete(fromIndex: indexPath.row)
+        default: return
+        }
+    }
+    
+}
---
+++ finance/Features/PortfolioManager/Implementations/PortfolioTableViewDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  PortfolioTableViewDelegate.swift
+//  finance
+//
+//  Created by Erik Radicheski da Silva on 21/01/22.
+//
+
+import UIKit
+
+class PortfolioTableViewDelegate: NSObject, UITableViewDelegate {
+    
+    weak var viewController: PortfolioViewProtocol?
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            self.viewController?.present(fromIndex: indexPath.row)
+        } else if tableView.isEditing && indexPath.row == 0 {
+            self.viewController?.insert()
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return indexPath.section == 0 ? .delete : .none
+    }
+    
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        return proposedDestinationIndexPath.section == 0 ? proposedDestinationIndexPath : sourceIndexPath
+    }
+
+}
