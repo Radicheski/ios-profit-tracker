43abf2e8665ff3670517ad76f406be8139f91e08
Radicheski <erik.radicheski@gmail.com>
2022-01-22 08:21:17 -0300

    Implemented PortfolioPresenterProtocol

---
+++ finance/Features/PortfolioManager/Implementations/PortfolioPresenter.swift
@@ -0,0 +1,24 @@
+//
+//  PortfolioPresenter.swift
+//  finance
+//
+//  Created by Erik Radicheski da Silva on 21/01/22.
+//
+
+import Foundation
+
+class PortfolioPresenter: NSObject, PortfolioPresenterProtocol {
+    
+    weak var view: PortfolioViewProtocol?
+    
+    func load(data: [Portfolio]) {
+        let viewModel = data.map { Portfolio.ViewModel(from: $0) }
+        self.view?.updateView(with: viewModel)
+    }
+    
+    func present(item: Portfolio) {
+        let viewModel = Portfolio.ViewModel(from: item)
+        self.view?.present(item: viewModel)
+    }
+    
+}
