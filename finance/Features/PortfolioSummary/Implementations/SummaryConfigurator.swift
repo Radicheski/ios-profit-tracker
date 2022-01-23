f5531e0d859ef26b91ce3481ac920e792ccfbece
Radicheski <erik.radicheski@gmail.com>
2022-01-23 11:46:43 -0300

    Implemented SummaryConfiguratorProtocol

---
+++ finance/Features/PortfolioSummary/Implementations/SummaryConfigurator.swift
@@ -0,0 +1,30 @@
+//
+//  SummaryConfigurator.swift
+//  finance
+//
+//  Created by Erik Radicheski da Silva on 22/01/22.
+//
+
+import Foundation
+import UIKit
+
+class SummaryConfigurator: SummaryConfiguratorProtocol {
+    
+    func build() -> SummaryViewProtocol {
+        let router = SummaryRouter()
+        let worker = SummaryWorker()
+        
+        let presenter = SummaryPresenter()
+        
+        let interactor = SummaryInteractor(presenter: presenter, worker: worker)
+        
+        let viewController = SummaryViewController(interactor: interactor, router: router)
+        presenter.view = viewController
+        router.view = viewController
+        
+        viewController.loadData()
+        
+        return viewController
+    }
+    
+}
