bedfe034b2a0986eb0697e65f9588c4d0a7e688a
Radicheski <erik.radicheski@gmail.com>
2022-01-23 11:45:58 -0300

    Implemented SummaryRouterProtocol

---
+++ finance/Features/PortfolioSummary/Implementations/SummaryRouter.swift
@@ -0,0 +1,12 @@
+//
+//  SummaryRouter.swift
+//  finance
+//
+//  Created by Erik Radicheski da Silva on 23/01/22.
+//
+
+class SummaryRouter: SummaryRouterProtocol {
+    var view: SummaryViewProtocol?
+    
+    
+}
