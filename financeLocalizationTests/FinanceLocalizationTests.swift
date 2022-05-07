fb176ad79ce0a49af66ead9da4a311e423784fb0
Radicheski <erik.radicheski@gmail.com>
2022-05-07 18:08:46 -0300

    Added unit tests for localization

---
+++ financeLocalizationTests/FinanceLocalizationTests.swift
@@ -0,0 +1,49 @@
+//
+//  FinanceLocalizationTests.swift
+//  FinanceLocalizationTests
+//
+//  Created by Erik Radicheski da Silva on 07/05/22.
+//
+
+import XCTest
+import finance
+
+class FinanceLocalizationTests: XCTestCase {
+
+  func testCommonStrings() {
+    for string in CommonStrings.allCases {
+      XCTAssertNotEqual(string.key, string.localizedString)
+    }
+  }
+
+  func testBrokerNoteStrings() {
+    for string in BrokerNoteStrings.allCases {
+      XCTAssertNotEqual(string.key, string.localizedString)
+    }
+  }
+
+  func testTransactionStrings() {
+    for string in TransactionStrings.allCases {
+      XCTAssertNotEqual(string.key, string.localizedString)
+    }
+  }
+
+  func testPortfolioUpdateStrings() {
+    for string in PortfolioUpdateStrings.allCases {
+      XCTAssertNotEqual(string.key, string.localizedString)
+    }
+  }
+
+  func testPortfolioManagerStrings() {
+    for string in PortfolioManagerStrings.allCases {
+      XCTAssertNotEqual(string.key, string.localizedString)
+    }
+  }
+
+  func testSummaryStrings() {
+    for string in SummaryStrings.allCases {
+      XCTAssertNotEqual(string.key, string.localizedString)
+    }
+  }
+
+}
